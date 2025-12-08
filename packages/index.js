0b95becba92227624b228088e59631509b5035e5
jatin <jatinjethava3125@gmail.com>
2025-12-08 13:26:18 +0530

    Add initial package files and example scripts for Node.js

---
+++ packages/index.js
@@ -0,0 +1,15 @@
+var colors = require('colors');
+
+console.log("Hello, World!".bgBlue.white);
+console.log("This is the index.js file.".bgGreen.black);
+console.log("Enjoy coding with colors!".bgRed.yellow);
+console.log("Have a great day!".bgCyan.magenta);
+console.log("Goodbye!".bgYellow.blue);
+console.log("You are doing great!".bgRed.yellow);
+console.log("Believe in yourself!".bgCyan.magenta);
+console.log("The sky is the limit!".bgYellow.blue);
+console.log("Coding is an adventure!".bgBlack.green);
+console.log("Embrace the challenges!".bgGray.cyan);
+console.log("Shine bright like a diamond!".bgRed.yellow);
+console.log("Let your code speak for itself!".bgCyan.magenta);
+console.log("Make a difference with your code!".bgYellow.blue);
