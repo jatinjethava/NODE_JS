be64800d7cb902c111db0b1e0e26c2fecf5218d1
jatin <jatinjethava3125@gmail.com>
2025-12-09 20:09:28 +0530

    update express routes to enhance responses and include dynamic content

--- express.js
+++ express.js
@@ -3,20 +3,33 @@ const app = express();
 const port = 8100;
 
 app.get('', (req, res) => {
-    res.send('Hello World!');
-    console.log('the data sent by server is :', req.query);
+    res.send(`<h1>Hello , ' + req.query.name + ' welcome to my first express app </h1>
+        <a href="/about">about</a><br>
+        <a href="/contact">contact</a><br>
+        <a href="/service">service</a>
+    `);
 });
 
 app.get('/about', (req, res) => {
-    res.send('this is about page!');
+    res.send({
+        name: 'Jatin Jethava',
+        age: 20,
+        city: 'Surat'
+    });
 });
 
 app.get('/contact', (req, res) => {
-    res.send('this is contact page!');
+    res.send(`this is contact page!
+        <br>
+        <a href="/">Go To Home</a>
+    `);
 });
 
 app.get('/service', (req, res) => {
-    res.send('this is service page!');
+    res.send(`this is service page!
+         <br>
+        <a href="/">Go To Home</a>
+        `);
 });
 
 app.listen(port, () => {
