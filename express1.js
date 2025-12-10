1c9acf8c467d80cc002139124d262c76021b2a59
jatin <jatinjethava3125@gmail.com>
2025-12-10 15:08:35 +0530

    add about and 404 HTML pages, update express routes for serving static files

--- express1.js
+++ express1.js
@@ -4,7 +4,18 @@ const port = 3000;
 const path = require('path');
 const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/about.html`);
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(publicPath, '404.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
