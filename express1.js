19fad5c49c958a51b44995f00b5aebd2dd92dbd5
jatin <jatinjethava3125@gmail.com>
2025-12-10 12:10:47 +0530

    add express server setup and initial HTML file

---
+++ express1.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
