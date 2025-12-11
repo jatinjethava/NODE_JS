a73620c947f455144394a30555a51855eaeec930
jatin <jatinjethava3125@gmail.com>
2025-12-11 16:37:17 +0530

    crud operation add in mongodb file

--- database_connection.js
+++ database_connection.js
@@ -3,7 +3,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'NODE_JS';
 const client = new MongoClient(url);
 
-async function get_data() {
+async function db_connection() {
     let result = await client.connect();
     let db = result.db(dbName);
     let collection = db.collection('users');
@@ -12,4 +12,4 @@ async function get_data() {
     // console.log(data);
 }
 
-module.exports = get_data;
+module.exports = db_connection;
