113d88e9ef9945cc8019940bac15a416c909d8c6
jatin <jatinjethava3125@gmail.com>
2025-12-12 19:29:42 +0530

    connect with database and insert data

--- mongoose.js
+++ mongoose.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+//Mongoose is used to connect your Node.js app with MongoDB and create models/schemas.
 
 const connectDB = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/NODE_JS');
+        // Define a schema and model
         const schema = new mongoose.Schema({ name: String });
         console.log('MongoDB connected successfully');
 
         const userModel = mongoose.model('users', schema);
+
+        // Example operation: create and save a new user
         let data = new userModel({ name: 'jethava' });
         let result = await data.save();
         console.log(result);
