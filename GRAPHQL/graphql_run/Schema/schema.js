a60b7d38ab0c50872cfe49fe7a0521312f67619e
jatin <jatinjethava3125@gmail.com>
2026-03-26 15:34:33 +0530

    complate examplle of graphql

--- GRAPHQL/graphql_run/Schema/schema.js
+++ GRAPHQL/graphql_run/Schema/schema.js
@@ -1,17 +1,93 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } = require("graphql");
+
+const user = [
+    { id: 1, name: "jatin jethava", email: "jatin@gmail.com" },
+    { id: 2, name: "yashdip jethava", email: "yashdip@gmail.com" }
+]
+
+const User = new GraphQLObjectType({
+    name: "userData",
+    fields: {
+        id: { type: GraphQLInt },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString }
+    }
+})
 
 const graphqlQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
+    name: 'GraphqlQuery', // get api in rest
     fields: {
-        hellow: {
-            type: GraphQLString,
-            resolve() {
-                return "hellow from graphQl"
+        user: {
+            type: User,
+            args: { id: { type: GraphQLInt } },
+            resolve(parent, args) {
+
+                return user.find(user => user.id == args.id);
             }
         }
     }
 })
 
+// mutetion example
+const MutetionUser = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUser: {
+            type: User,
+            args: {
+                id: { type: GraphQLInt },
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+            },
+            resolve(perent, args) {
+                const data = {
+                    id: user.length + 1 + "",
+                    name: args.name,
+                    email: args.email
+                }
+
+                user.push(data);
+                return data;
+            }
+        },
+        updateUser: {
+            type: User,
+            args: {
+                id: { type: GraphQLInt },
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+            },
+            resolve(perent, args) {
+
+                const userId = user.find(user => user.id == args.id);
+                if (userId) {
+
+                    userId.name = args.name || user.name,
+                        userId.email = args.email || user.email
+
+                    console.log(userId);
+                    return userId;
+                }
+                throw new Error("User not found");
+            }
+        },
+        deleteUser: {
+            type: User,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve(perent, args) {
+
+                const index = user.findIndex(user => user.id == args.id);
+                if (index === -1) throw new Error("User not found");
+                return user.splice(index, 1)[0];
+            }
+        }
+    }
+})
+
+
 module.exports = new GraphQLSchema({
-    query: graphqlQuery
+    query: graphqlQuery,
+    mutation: MutetionUser
 })
