9cecbaadbaff8dec637b0e424725e09994a6666d
jatin <jatinjethava3125@gmail.com>
2026-04-17 12:13:26 +0530

    Add WebSocket chat application with dependency injection and file streaming
    - Implemented a WebSocket server using Socket.IO for real-time chat functionality.
    - Created a frontend HTML interface for the chat application.
    - Added file streaming capabilities to copy content from one file to another.
    - Introduced Dependency Injection concept in the application for better code maintainability.

--- Stream/index.js
+++ Stream/index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const status = require("express-status-monitor");
 const app = express()
-const fs = require("fs");
+const { createReadStream, createWriteStream } = require("fs");
 const path = require("path");
 const port = 3000
+
+const inputFile = path.join(__dirname, "simple.txt");
+const outputFile = path.join(__dirname, "copy.txt");
+
+const readStream = createReadStream(inputFile, {
+    encoding: "utf-8",
+    highWaterMark: 16 * 1024
+});
+
+const writeStream = createWriteStream(outputFile);
+
+readStream.on("data", (chunk) => {
+    console.log("chunk received :", chunk);
+    console.log("Buffer :", Buffer.from(chunk));
+    writeStream.write(chunk);
+});
+
+writeStream.on("finish", () => {
+    console.log("file copy completed");
+});
 
 app.use(status());
 
---
+++ WebSocket/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require("cors");
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+const port = 8100;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('Public'));
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/Public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('connected: ' + socket.id);
+    socket.on('disconnect', () => {
+        console.log('disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
